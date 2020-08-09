e5281701a539c661e19a8417de58e2dda2ccca79
Jakub Bartczuk <bartczukkuba@gmail.com>
2020-08-09 15:42:39 +0200

    leaner requirements for heroku

--- streamlit/heroku_setup.sh
+++ streamlit/heroku_setup.sh
@@ -1,4 +1,5 @@
 mkdir -p .streamlit
 cat streamlit/config.toml | sed s/PORT/$PORT/ - > .streamlit/config.toml
 cp streamlit/credentials.toml .streamlit/credentials.toml
+pip install torch==1.5.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
 nbdev_build_lib; pip install -e .
