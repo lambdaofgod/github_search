53f45dc2c84c315a02fcf0a451e81c05b92bce87
lambdaofgod <bartczukkuba@gmail.com>
2021-02-27 15:06:12 +0100

    neptune experiment and model files tracking

--- github_search/neptune_util.py
+++ github_search/neptune_util.py
@@ -1,10 +1,14 @@
 import neptune
 
 
-def init_neptune(experiment_name, params, **kwargs):
+def init_neptune_project():
     neptune_token = open('.neptune_token', 'r').read()
-    neptune.init('lambdaofgod/github-search', api_token=neptune_token)
-    neptune.create_experiment(
+    return neptune.init('lambdaofgod/github-search', api_token=neptune_token)
+
+
+def init_neptune_experiment(experiment_name, params, **kwargs):
+    init_neptune_project()
+    return neptune.create_experiment(
         experiment_name,
         params=params,
         upload_source_files=["**/*.ipynb"]
