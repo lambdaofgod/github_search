bccb5657468b57e9281acfafc1630098cf6cb1d5
lambdaofgod <bartczukkuba@gmail.com>
2021-06-21 12:47:11 +0200

    package files

---
+++ github_search/None.py
@@ -0,0 +1,68 @@
+
+
+# Cell
+import os
+import pandas as pd
+import numpy as np
+import tqdm
+
+import ast
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from sklearn import feature_extraction
+import matplotlib.pyplot as plt
+import gensim
+
+
+torch.manual_seed(1)
+
+# Cell
+import os
+import pandas as pd
+import numpy as np
+import tqdm
+
+import ast
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from sklearn import feature_extraction
+import matplotlib.pyplot as plt
+import gensim
+
+
+torch.manual_seed(1)
+
+# Cell
+import pandas as pd
+import numpy as np
+from github_search import python_tokens
+from mlutil.feature_extraction import embeddings
+from mlutil import prototype_selection
+
+# Cell
+import pprint
+import numpy as np
+import pandas as pd
+import requests
+import sentence_transformers
+
+from github_search import token2vec_zsl, paperswithcode_tasks
+import pandas as pd
+import ast
+
+# Cell
+import pprint
+import numpy as np
+import pandas as pd
+import requests
+import sentence_transformers
+
+from github_search import token2vec_zsl, paperswithcode_tasks
+import pandas as pd
+import ast
