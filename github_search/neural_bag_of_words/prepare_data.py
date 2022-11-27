9a899cd2ff5095bb17eb5d4ec83da13f17fdd409
lambdaofgod <bartczukkuba@gmail.com>
2022-11-27 12:53:21 +0100

    prepare NBOW data

--- github_search/neural_bag_of_words/prepare_data.py
+++ github_search/neural_bag_of_words/prepare_data.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from github_search import python_tokens, utils
+from github_search.neural_bag_of_words.data import prepare_dependency_texts
 
 
 def tokenize_python_code(code_text):
@@ -17,13 +18,36 @@ def get_dependency_texts(dependency_records_df):
     dependency_records_df = dependency_records_df[
         dependency_records_df["source"] != "<ROOT>"
     ]
-    return dependency_records_df.groupby("repo")["destination"].agg(" ".join)
+    return dependency_records_df.groupby("repo")["destination"].agg(" ".join).reset_index()
 
 
-def prepare_dependency_data(upstream, product):
+def get_dependency_nbow_dataset(paperswithcode_df, df_dependency_corpus):
+    dep_texts_with_tasks_df = (
+        paperswithcode_df[["repo", "tasks"]]
+        .merge(df_dependency_corpus, on="repo")
+        .dropna()
+    )
+    return dep_texts_with_tasks_df
+
+
+def prepare_dependency_data_corpus(upstream, product):
     dependency_records_path = str(upstream["dependency_graph.prepare_records"])
     dependency_records_df = pd.read_feather(dependency_records_path)
     dep_texts = get_dependency_texts(dependency_records_df)
-    pd.DataFrame(dep_texts).reset_index().to_feather(product["text"])
-    pd.DataFrame(dep_texts).reset_index(drop=True).to_csv(product["raw_text"], index=False, header=False)
+    dep_texts = dep_texts.dropna().rename({"destination": "dependencies"}, axis=1)
+    pd.DataFrame(dep_texts).reset_index().to_parquet(product["text"])
+    pd.DataFrame(dep_texts).reset_index(drop=True).to_csv(
+        product["raw_text"], index=False, header=False
+    )
 
+
+def prepare_nbow_dataset(upstream, product):
+    df_dependency_corpus = pd.read_parquet(
+        str(upstream["nbow.prepare_dependency_data_corpus"]["text"])
+    )
+    for split_name in ["train", "test"]:
+        df_paperswithcode = pd.read_csv(
+            str(upstream["prepare_repo_train_test_split"][split_name])
+        )
+        df_corpus = get_dependency_nbow_dataset(df_paperswithcode, df_dependency_corpus)
+        df_corpus.to_parquet(str(product[split_name]))
