31bc4d01671fdeec5283c71c6b14127b99da7e1b
lambdaofgod <bartczukkuba@gmail.com>
2023-04-30 15:50:07 +0200

    ops code

--- org/promptify_runner.py
+++ org/promptify_runner.py
@@ -11,6 +11,7 @@ import tqdm
 import json
 
 from prompting_config import PromptDataConfig
+
 # -
 
 from record_writer import JsonWriterContextManager
@@ -20,51 +21,64 @@ import clearml
 np.random.seed(seed=0)
 
 
-
 import ast
 
 # +
+import logging
+
+logging.basicConfig(level="INFO")
 
 
 class ContextLoader(BaseModel):
-    
+
     data_path: str
     used_cols: List[str] = ["dependencies", "tasks", "repo"]
     truncation_kwargs: Dict[str, int] = {"dependencies": 10}
-    
+
     def _truncate_fields(self, row):
         for field in self.truncation_kwargs.keys():
-            row[field] = " ".join(row[field][:500].split(" ")[:self.truncation_kwargs[field]])
+            row[field] = " ".join(
+                row[field][:500].split(" ")[: self.truncation_kwargs[field]]
+            )
 
     def _get_pandas_dicts(self, df):
         for _, row in df.iterrows():
             row_dict = dict(row)
             self._truncate_fields(row_dict)
             yield row_dict
-    
+
     def load_contexts(self, n_shots=2, n_samples=1000):
         df = pd.read_parquet(self.data_path).drop(["count"], axis=1)[self.used_cols]
-        context_records = zip(*(
-            self._get_pandas_dicts(df.sample(n_samples))
-            for i in range(n_shots))
+        context_records = zip(
+            *(self._get_pandas_dicts(df.sample(n_samples)) for i in range(n_shots))
         )
         return context_records
 
     def load_predicted_df(self, n_samples=1000):
         df = pd.read_parquet(self.data_path).drop(["count"], axis=1)[self.used_cols]
-        if n_samples is None: 
+        if n_samples is None:
             return df
         else:
             return df.sample(n_samples)
-    
+
     def load_prompt_infos(self, n_samples):
         predicted_df = self.load_predicted_df(n_samples=n_samples)
         predicted_records = self._get_pandas_dicts(predicted_df)
-        contexts = self.load_contexts(n_samples=n_samples if n_samples is None else len(predicted_df))
-        return (PromptInfo(repo_records=ctx, predicted_repo_record=dict(pred), true_tasks=ast.literal_eval(pred["tasks"])) for (ctx, pred) in zip(contexts, predicted_records))
+        contexts = self.load_contexts(
+            n_samples=n_samples if n_samples is None else len(predicted_df)
+        )
+        return (
+            PromptInfo(
+                repo_records=ctx,
+                predicted_repo_record=dict(pred),
+                true_tasks=ast.literal_eval(pred["tasks"]),
+            )
+            for (ctx, pred) in zip(contexts, predicted_records)
+        )
 
 
 # -
+
 
 def run_progress_barred_loop(fn, record_writer_cls, inputs, total, writer_kwargs):
     with record_writer_cls(**writer_kwargs) as writer:
@@ -78,33 +92,44 @@ def run_progress_barred_loop(fn, record_writer_cls, inputs, total, writer_kwargs
 # +
 from clearml import Dataset, Task
 import jsonlines
+from github_search.experiment_managers import ClearMLExperimentManager
+
+
+def get_experiment_manager(project_name, task_name, config=dict()):
+    return ClearMLExperimentManager(
+        project_name=project_name, task_name=task_name, config=config
+    )
 
 
 class Main:
-    
-    def add_as_clearml_dataset(self, path, **kwargs):
-        dataset = Dataset.create(**kwargs)
-        dataset.sync_folder(local_path=path)
-        dataset.upload()
-        
-    def add_clearml_task_output(self, task_name, output_file):
-        task = Task(project_name="github_search_llms", task_name=task_name)
-        task.upload_artifact('prediction_results', output_file)
-        
+
+    project_name = "github_search"
+
     def sample_data(
         self,
         n_samples=100,
         pq_data_path="../output/nbow_data_test.parquet",
         out_data_path="../output/prompt_infos.jsonl",
-        dset_kwargs=dict(dataset_project='github_search_llms', dataset_name='prompt_info_sample')
+        dset_kwargs=dict(
+            dataset_project="github_search_llms", dataset_name="prompt_info_sample"
+        ),
     ):
-        prompt_infos = ContextLoader(data_path=pq_data_path).load_prompt_infos(n_samples=n_samples)
-        pd.DataFrame.from_records(map(dict, prompt_infos)).to_json(out_data_path, orient="records", lines=True)
-        self.add_as_clearml_dataset(out_data_path, dset_kwargs)
+        prompt_infos = ContextLoader(data_path=pq_data_path).load_prompt_infos(
+            n_samples=n_samples
+        )
+        sample_df = pd.DataFrame.from_records(map(dict, prompt_infos))
+        sample_df.to_json(out_data_path, orient="records", lines=True)
+        with get_experiment_manager(
+            project_name=self.project_name, task_name="sample_prompt_infos"
+        ) as mgr:
+            mgr.add_artifact(
+                "sample_prompt_infos", sample_df, metadata={"n_samples": n_samples}
+            )
+        logging.info("created samples")
 
     def run_model(
         self,
-        model_path="/home/kuba/Projects/forks/text-generation-webui/models/llama-13b-hf/",
+        model_name="dvruette/oasst-pythia-6.9b-4000-steps",
         prompt_template_name="md_prompt.jinja",
         templates_path="prompt_templates",
         data_path="../output/prompt_infos.jsonl",
@@ -113,27 +138,32 @@ class Main:
         fake=False,
     ):
         print(f"loading data from {data_path}...")
-        with jsonlines.open(data_path, 'r') as f:
+        with jsonlines.open(data_path, "r") as f:
             prompt_infos = [PromptInfo(**d) for d in f]
-        model_nm = P(model_path).parent.name
+        model_nm = P(model_name.replace("/", "-")).parent.name
         out_path = P(out_dir) / (model_nm + ".jsonl")
-        out_path.parent.mkdir(exist_ok=True)
+        out_path.parent.mkdir(exist_ok=True, parents=True)
         if out_path.exists():
             out_path.unlink()
         print(f"write to {out_path}...")
         writer_kwargs = {"file_path": out_path}
         prompter_wrapper = PrompterWrapper.create(
-            model_path, templates_path, prompt_template_name, use_fake_model=fake
+            model_name, templates_path, prompt_template_name, use_fake_model=fake
         )
 
-        run_progress_barred_loop(
-            prompter_wrapper.get_dict_with_generated_text,
-            JsonWriterContextManager,
-            prompt_infos,
-            total=n_samples,
-            writer_kwargs=writer_kwargs,
-        )
-        self.add_clearml_task_output(f"prediction_{model_nm}", out_path)
+        with get_experiment_manager(
+            self.project_name,
+            task_name="document_expansion",
+            config={"model_name": model_name},
+        ) as mgr:
+            run_progress_barred_loop(
+                prompter_wrapper.get_dict_with_generated_text,
+                JsonWriterContextManager,
+                prompt_infos,
+                total=n_samples,
+                writer_kwargs=writer_kwargs,
+            )
+            mgr.add_artifact("generated_texts", out_path, metadata={"total": n_samples})
 
 
 # -
--- org/promptify_utils.py
+++ org/promptify_utils.py
@@ -2,42 +2,64 @@ from prompting import PromptInfo
 from promptify import Prompter
 from mlutil.text import rwkv_utils
 from pydantic import BaseModel, Field
-from transformers import BitsAndBytesConfig, AutoModelForCausalLM, AutoTokenizer, pipeline as hf_pipeline
+from transformers import (
+    BitsAndBytesConfig,
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    pipeline as hf_pipeline,
+)
 from pathlib import Path
+import torch
 
 from transformers import Text2TextGenerationPipeline
 from typing import Any
 
 
-# +
-class HFModelWrapper(BaseModel):
-    pipeline: Any
-    
+class HFModelWrapper:
+    def __init__(self, model, tokenizer):
+        self.model = model
+        self.tokenizer = tokenizer
+
+    @staticmethod
+    def load_from_path(model_path):
+        model = AutoModelForCausalLM.from_pretrained(
+            model_path, torch_dtype=torch.float16
+        )
+        model.cuda()
+        tokenizer = AutoTokenizer.from_pretrained(model_path)
+        return HFModelWrapper(model, tokenizer)
+
     def run(self, prompts, max_tokens):
-        return self.pipeline(text_inputs=prompts,  max_new_tokens=max_tokens)
-        
-    class Config:
-        arbitrary_types_allowed = True
+        inputs = self.tokenizer.encode(prompts[0], return_tensors="pt").cuda()
+        input_length = inputs.shape[-1]
+        outputs = self.llama_model.generate(inputs, max_new_tokens=max_tokens)[
+            :, input_length:
+        ]
+        return [self.tokenizer.decode(outputs[0])]
+
+    @staticmethod
+    def create_llama(model_path):
+        tokenizer = AutoTokenizer.from_pretrained(model_path)
+        bb_config = BitsAndBytesConfig(load_in_8bit=True)
+        llama_model = AutoModelForCausalLM.from_pretrained(
+            model_path, quantization_config=bb_config, device_map="auto"
+        )
+        return HFModelWrapper(llama_model, tokenizer)
+
 
 class FakeModel:
     def run(self, *args, **kwargs):
         return ["foo"]
 
 
-# -
-
 def load_model(model_path):
     if "llama" in model_path:
-        model_path = Path(model_path).expanduser()
-        tokenizer = AutoTokenizer.from_pretrained(model_path)
-        bb_config = BitsAndBytesConfig(load_in_8bit=True)
-        llama_model = AutoModelForCausalLM.from_pretrained(
-            model_path, quantization_config=bb_config, device_map="auto"
-        )
-        return HFModelWrapper(pipeline=hf_pipeline(task="text-generation",model=llama_model, tokenizer=tokenizer))
-    else:
+        return HFModelWrapper.create_llama(model_path)
+    elif "rwkv" in model_path:
         pipeline = rwkv_utils.RWKVPipelineWrapper.load(model_path=model_path)
         return rwkv_utils.RWKVPromptifyModel(pipeline=pipeline)
+    else:
+        return HFModelWrapper.load_from_path(model_path)
 
 
 class PrompterWrapper(BaseModel):
