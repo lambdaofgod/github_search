299296eb81bd424ba13f64dea64079a184562203
Jakub 'lambdaofgod' Bartczuk <bartczukkuba@gmail.com>
2023-11-20 21:18:46 +0100

    running project with poetry

--- github_search/pipelines/pipeline.py
+++ github_search/pipelines/pipeline.py
@@ -1,7 +1,6 @@
 import zenml
-import fire
 import pandas as pd
-from typing import Tuple
+from typing import Tuple, Annotated
 from github_search.pipelines.configs import EvaluationConfig, SearchDataConfig
 from github_search.pipelines.steps import (
     expand_documents_step,
@@ -19,11 +18,18 @@ from github_search.pipelines.configs import PipelineConfig
 
 
 @pipeline
-def generation_pipeline():
+def generation_pipeline(
+    sampling,
+    generation_method,
+    prompting_method,
+):
     pipeline_config = PipelineConfig.load_from_paths(
         "generation pipeline",
         "github_search",
-        "../../data/paperswithcode_with_tasks.csv",
+        generation_method=generation_method,
+        prompting_method=prompting_method,
+        sampling=sampling,
+        search_config_path="conf/pipeline/search.yaml",
     )
     config = pipeline_config.generation_config
     prompt_infos = sample_data_step(
@@ -32,29 +38,26 @@ def generation_pipeline():
     generated_texts_df, failures = expand_documents_step(
         dict(config.generation_config), dict(config.prompt_config), prompt_infos
     )
-    generation_eval_df = evaluate_generated_texts(
-        generated_texts_df,
-        pipeline_config.paperswithcode_path,
-        EvaluationConfig(reference_text_col="true_tasks"),
-    )
 
 
-@step
-def load_generation_pipeline_dfs() -> Tuple[pd.DataFrame, pd.DataFrame]:
+@step(enable_cache=True)
+def load_generation_pipeline_df() -> Annotated[pd.DataFrame, "generated_texts_df"]:
     client = zenml.client.Client()
     run = client.get_pipeline("generation_pipeline").runs[0]
     generated_texts_df = (
         run.steps["expand_documents_step"].outputs["generated_texts_df"].load()
     )
-    generation_eval_df = (
-        run.steps["evaluate_generated_texts"].outputs["generation_metrics_df"].load()
-    )
-    return generated_texts_df, generation_eval_df
+    return generated_texts_df
 
 
 @pipeline
 def metrics_pipeline():
-    generated_texts_df, generation_eval_df = load_generation_pipeline_dfs()
+    generated_texts_df = load_generation_pipeline_df()
+    generation_eval_df = evaluate_generated_texts(
+        generated_texts_df,
+        EvaluationConfig(reference_text_col="true_tasks"),
+    )
+
     ir_config = load_config_yaml_key(
         InformationRetrievalEvaluatorConfig, "conf/ir_config.yaml", "nbow"
     )
@@ -63,19 +66,15 @@ def metrics_pipeline():
     )
 
     search_df = prepare_search_df(search_data_config, generated_texts_df)
-    ir_df = evaluate_information_retrieval(search_df, ir_config)
     get_ir_experiments_results(search_df, generation_eval_df)
 
 
-class CLI:
-    @staticmethod
-    def run_generation():
-        generation_pipeline()
+def run_generation(
+    sampling="no_sampling",
+    generation_method="api_rwkv",
+    prompting_method="few_shot_markdown",
+):
+    generation_pipeline(sampling, generation_method, prompting_method)
 
-    @staticmethod
-    def run_metrics():
-        metrics_pipeline()
-
-
-if __name__ == "__main__":
-    CLI().run_metrics()
+def run_metrics():
+    metrics_pipeline()
