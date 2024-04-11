4e96a07f1f013a42976d1ee566400db5f3b513f5
Jakub 'lambdaofgod' Bartczuk <bartczukkuba@gmail.com>
2024-04-11 19:44:15 +0200

    added task and repo sampling, and selecting repos that have both code and readmes

--- github_search/pipelines/steps.py
+++ github_search/pipelines/steps.py
@@ -10,21 +10,22 @@ from github_search.pipelines.postprocessing import GenerationPostprocessor
 from tgutil.configs import PromptConfig, load_config_from_dict
 from tgutil.prompting import ContextPromptInfo
 from tgutil.prompting_runner import DocumentExpander
-from zenml import step
+from github_search.samplers import TaskSampler, RepoSampler
 
 
-@step(enable_cache=False)
 def expand_documents_step(
     text_generation_config: dict, prompt_config: dict, prompt_infos: List[dict]
 ) -> Tuple[
-    Annotated[pd.DataFrame, "raw_generated_texts_df"], Annotated[List[dict], "failures"]
+    Annotated[pd.DataFrame,
+              "raw_generated_texts_df"], Annotated[List[dict], "failures"]
 ]:
     logging.info("expanding documents")
     logging.info(f"using text generation config: {text_generation_config}")
     logging.info(f"using prompt config: {prompt_config}")
     text_generation_config = load_config_from_dict(text_generation_config)
     prompt_config = PromptConfig(**prompt_config)
-    parsed_prompt_infos = [ContextPromptInfo.parse_obj(pi) for pi in prompt_infos]
+    parsed_prompt_infos = [
+        ContextPromptInfo.parse_obj(pi) for pi in prompt_infos]
     raw_generated_texts_df, failures = DocumentExpander(
         text_generation_config=text_generation_config, prompts_config=prompt_config
     ).expand_documents(parsed_prompt_infos)
@@ -38,14 +39,12 @@ def expand_documents_step(
     return raw_generated_texts_df, failures
 
 
-@step(enable_cache=False)
 def postprocess_generated_texts(
     raw_generated_texts_df: pd.DataFrame,
 ) -> Annotated[pd.DataFrame, "generated_texts_df"]:
     return GenerationPostprocessor.run(raw_generated_texts_df)
 
 
-@step(enable_cache=False)
 def sample_data_step(
     prompt_config: dict, sampling_config: dict
 ) -> Annotated[List[dict], "prompt_infos"]:
@@ -54,4 +53,31 @@ def sample_data_step(
 
     sampling_config = SamplingConfig(**sampling_config)
     prompt_config = PromptConfig(**prompt_config)
-    return DocumentExpander.sample_data(prompt_config, sampling_config)
+    return [
+        pinfo.dict()
+        for pinfo in DocumentExpander.sample_data(prompt_config, sampling_config)
+    ]
+
+
+def select_repos_with_all_data(repos_df_path, python_code_path, output_path):
+    repos_df = pd.read_json(repos_df_path)
+    python_code_df = pd.read_parquet(python_code_path)
+    repos_with_all_data_df = repos_df[repos_df["repo"].isin(
+        python_code_df["repo_name"])]
+    repos_with_all_data_df.to_json(output_path, orient="records", lines=True)
+
+
+def create_repos_sample(repos_df_path, output_path, sampled_tasks=100, repos_per_task=20, min_task_size=250, max_task_size=2500, min_repo_tasks=4):
+    logging.basicConfig(level=logging.INFO)
+    repos_df = pd.read_json(repos_df_path, orient="records", lines=True)
+    if type(repos_df["tasks"].iloc[0]) is str:
+        repos_df["tasks"] = repos_df["tasks"].apply(ast.literal_eval)
+    tasks_sample = TaskSampler.sample_tasks_from_lists(
+        repos_df["tasks"], sample_size=sampled_tasks, min_size=min_task_size, max_size=max_task_size)
+    logging.info(f"Sampled {len(tasks_sample)} tasks.")
+    sampled_repos_df = RepoSampler.sample_repos(
+        repos_df, tasks_sample, sample_size_per_task=repos_per_task, min_repo_tasks=min_repo_tasks)
+    sampled_repos_df["query_tasks"] = sampled_repos_df["tasks"].apply(
+        lambda ts: [t for t in ts if t in tasks_sample])
+    logging.info(f"Sampled {len(sampled_repos_df)} repos.")
+    sampled_repos_df.to_json(output_path, orient="records", lines=True)
--- github_search/samplers.py
+++ github_search/samplers.py
@@ -2,6 +2,35 @@ import abc
 import pandas as pd
 from pydantic import BaseModel
 from typing import Optional
+import logging
+import ast
+
+
+class DeterministicSampler:
+    """
+    this class is used to make sampling deterministic
+    we do this by specifying a 'random' function
+    which will be used for sorting samples
+    """
+
+    @classmethod
+    def string_pseudohash(cls, s):
+        return sum([ord(c) for c in s])
+
+    @classmethod
+    def sample(cls, strings, sample_size):
+        if type(strings) == pd.Series:
+            if strings.dtype == "object":
+                strings = strings.values
+            else:
+                strings = strings.index
+        pseudohashes = [cls.string_pseudohash(s) for s in strings]
+        strings_by_pseudohashes = sorted(
+            zip(strings, pseudohashes), key=lambda x: x[1])
+        if len(strings_by_pseudohashes) < sample_size:
+            logging.warning(
+                "Sample size is larger than the number of strings. Returning all strings.")
+        return [s for s, _ in strings_by_pseudohashes[:sample_size]]
 
 
 class TaskSampler:
@@ -10,25 +39,32 @@ class TaskSampler:
         return task_lists.explode().value_counts()
 
     @classmethod
-    def sample_tasks_from_lists(cls, task_lists, sample_size: int, min_size: int, max_size: int = 10000):
+    def sample_tasks_from_lists(cls, task_lists, sample_size: int, min_size: int, max_size: int = 10000, sample_fn=DeterministicSampler.sample):
         task_counts = cls.get_task_counts(task_lists)
         valid_tasks = task_counts[(task_counts >= min_size) & (
             task_counts <= max_size)]
-        return valid_tasks.sample(sample_size)
+        return sample_fn(valid_tasks, sample_size)
 
 
 class RepoSampler(BaseModel):
     repo_col: str = "repo"
     tasks_col: str = "tasks"
 
-    @ classmethod
-    def sample_repos(cls, repos_df, sample_tasks, sample_size_per_task: int) -> pd.DataFrame:
+    @classmethod
+    def sample_repos(cls, repos_df, sample_tasks, sample_size_per_task: int, min_repo_tasks: int = 1, sample_fn=DeterministicSampler.sample) -> pd.DataFrame:
         if type(sample_tasks) is pd.Series:
             sample_tasks = sample_tasks.index
         repo_names = []
         for task in sample_tasks:
-            task_repos = repos_df[repos_df["tasks"].apply(
-                lambda ts: task in ts)]["repo"]
-            repo_names.extend(task_repos.sample(sample_size_per_task))
+            task_repos_mask = repos_df["tasks"].apply(
+                lambda ts: task in ts)
+            min_repo_tasks_mask = repos_df["tasks"].apply(
+                lambda ts: len(ts) >= min_repo_tasks)
+            task_repos = repos_df[task_repos_mask &
+                                  min_repo_tasks_mask]["repo"]
+            if len(task_repos) < sample_size_per_task:
+                logging.warning(
+                    f"Task {task} has less than {sample_size_per_task} repos. Sampling all.")
+            repo_names.extend(sample_fn(task_repos, sample_size_per_task))
         repo_names = list(set(repo_names))
         return repos_df[repos_df["repo"].isin(repo_names)]
