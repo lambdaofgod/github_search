e900e0ec8c5c55add245d06423112fe6095e945c
Jakub Bartczuk <bartczukkuba@gmail.com>
2025-07-06 14:06:22 +0200

    refactor: modify nodes dataframe to create separate rows per node-repo pair

--- org/graph_records.jl
+++ org/graph_records.jl
@@ -41,9 +41,16 @@ nodes_dict = Dict([(nodes[i], i) for i in 1:length(nodes)])
 src_idxs = [nodes_dict[df[i,:source]] for i in tqdm(1:size(df)[1])]
 dst_idxs = [nodes_dict[df[i,:destination]] for i in tqdm(1:size(df)[1])]
 
-# Create nodes dataframe with index, name, and repos
-nodes_df = DataFrame([(i, nodes[i], join(node_to_repos[nodes[i]], ",")) for i in 1:length(nodes)])
-nodes_df = rename!(nodes_df, [:index, :name, :repos])
+# Create expanded nodes dataframe with one row per node-repo pair
+expanded_nodes = []
+for i in 1:length(nodes)
+    node_name = nodes[i]
+    for repo in node_to_repos[node_name]
+        push!(expanded_nodes, (i, node_name, repo))
+    end
+end
+nodes_df = DataFrame(expanded_nodes)
+nodes_df = rename!(nodes_df, [:index, :name, :repo])
 
 edges_df = DataFrame(index=df[!,:index], src=src_idxs, dst=dst_idxs, edge_type=df[!, :edge_type], repo=df[!, :repo])
 CSV.write("../output/dependency_records/nodes.csv", nodes_df)
