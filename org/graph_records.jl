9f7e2a543a517e3cff96efa3036867c323586c3f
Jakub Bartczuk <bartczukkuba@gmail.com>
2025-07-06 14:06:23 +0200

    refactor: optimize nodes_df creation with pre-allocation and sizehint

--- org/graph_records.jl
+++ org/graph_records.jl
@@ -42,17 +42,27 @@ nodes_dict = Dict([(nodes[i], i) for i in 1:length(nodes)])
 src_idxs = [nodes_dict[df[i,:source]] for i in tqdm(1:size(df)[1])]
 dst_idxs = [nodes_dict[df[i,:destination]] for i in tqdm(1:size(df)[1])]
 
-# Create expanded nodes dataframe with one row per node-repo pair
+# Count total number of rows needed for nodes_df
+println("counting total node-repo pairs")
+total_rows = 0
+for i in 1:length(nodes)
+    node_name = nodes[i]
+    total_rows += length(node_to_repos[node_name])
+end
+
+# Pre-allocate the dataframe
 println("creating nodes dataframe")
-expanded_nodes = []
+nodes_df = DataFrame(index = Int[], name = String[], repo = String[])
+sizehint!(nodes_df, total_rows)
+
+# Fill the dataframe
+println("filling nodes dataframe")
 for i in ProgressBar(1:length(nodes))
     node_name = nodes[i]
     for repo in node_to_repos[node_name]
-        push!(expanded_nodes, (i, node_name, repo))
+        push!(nodes_df, (i, node_name, repo))
     end
 end
-nodes_df = DataFrame(expanded_nodes)
-nodes_df = rename!(nodes_df, [:index, :name, :repo])
 
 edges_df = DataFrame(index=df[!,:index], src=src_idxs, dst=dst_idxs, edge_type=df[!, :edge_type], repo=df[!, :repo])
 CSV.write("../output/dependency_records/nodes.csv", nodes_df)
