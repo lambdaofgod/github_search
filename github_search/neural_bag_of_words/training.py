405c5a60c48ed2648fe310a7f6cfb6a319f9da15
Jakub Bartczuk <bartczukkuba@gmail.com>
2025-09-16 21:44:20 +0200

    unused files

--- github_search/neural_bag_of_words/training.py
+++
@@ -1,5 +0,0 @@
-import itertools
-
-import pytorch_lightning as pl
-import torch
-import torch.utils
--- github_search/sentence_embeddings/training.py
+++
@@ -1,8 +0,0 @@
-def make_sentence_similarity_pairs(record):
-    return [(label, record["description"]) for label in record["labels"]] + [
-        (record["full_name"], record["description"])
-    ]
-
-
-def load_datasets():
-    pass
